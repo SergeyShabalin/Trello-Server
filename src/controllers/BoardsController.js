509c7956b69b46796d1f23ccd1d4c708565be498
ssworkss <obaldevshiy@yandex.ru>
2022-11-24 13:03:34 +0300

    update board

--- src/controllers/BoardsController.js
+++ src/controllers/BoardsController.js
@@ -1,11 +1,22 @@
 const boardsModel = require('../models/boards-model')
 const columnsModel = require("../models/columns-model");
+const ColumnsModel = require("../models/columns-model");
 
 class BoardsController {
+
     async getAllBoards(req, res, next) {
         try {
-            const boardData = await boardsModel.find({_id: req.params.id})
+            const boardData = await boardsModel.find({})
             return res.json(boardData)
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    async getBoard(req, res, next) {
+        try {
+            const boardData = await boardsModel.find({_id: req.params.id})
+            return res.json(boardData[0])
         } catch (e) {
             next(e);
         }
@@ -13,10 +24,22 @@ class BoardsController {
 
     async newBoard(req, res, next) {
         try {
-            const columnNew = new boardsModel({title: 'доска 1'})
-            await columnNew.save()
-            console.log(boardsModel)
-            return res.json(columnNew)
+            const boardNew = new boardsModel(req.body)
+            await boardNew.save()
+            return res.json(boardNew)
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    async updateBoard(req, res, next) {
+        try {
+            const currentBoard = await boardsModel.findOne({_id: req.params.id})
+            if (req.body.title === currentBoard.title) {
+                console.log('обновление не требуется')
+            } else{
+                const newCurrentBoard =  await boardsModel.updateOne({_id: req.params.id}, {title: req.body.title})
+            return res.json(newCurrentBoard)}
         } catch (e) {
             next(e);
         }
--- src/routes/boards.js
+++ src/routes/boards.js
@@ -2,9 +2,10 @@ const Router = require('express').Router;
 const controller = require('../controllers/BoardsController')
 const BoardsRouter = new Router();
 //get post delete patch
-BoardsRouter.get('/:id', controller.getAllBoards)
-BoardsRouter.post('/new', controller.newBoard)
-
+BoardsRouter.get('/', controller.getAllBoards)
+BoardsRouter.get('/:id', controller.getBoard)
+BoardsRouter.post('/', controller.newBoard)
+BoardsRouter.patch('/:id', controller.updateBoard)
 
 
 module.exports = BoardsRouter
