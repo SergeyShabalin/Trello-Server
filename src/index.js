64addeb2ead2993c4a88a2eccfaf307b1caa6608
ssworkss <obaldevshiy@yandex.ru>
2023-03-21 16:48:16 +0300

    correct socket but double information in refresh

--- src/index.js
+++ src/index.js
@@ -2,35 +2,35 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
+const app = express()
+const http = require("http")
+const { Server } = require("socket.io");
+const server = http.createServer(app)
+
 const router = require('./routes/index')
 const ColumnsRouter = require('./routes/columns')
 const CardsRouter = require('./routes/cards')
 const BoardRouter = require('./routes/boards')
 const UserRouter = require('./routes/users')
+const ChecklistRouter = require('./routes/checklist')
 const CardsController = require('../src/controllers/CardsController')
 const ColumnsController = require('../src/controllers/ColumnsController')
-const app = express()
-const http = require('http')
-const ChecklistRouter = require('./routes/checklist')
 
-const server = http.createServer(app)
-const {Server} = require("socket.io");
- const io = new Server(server, {
+
+const io = new Server(server, {
     cors: {
         credentials: true,
+        methods: ['GET', 'POST'],
         origin: process.env.CLIENT_URL
     }
 })
 
 
-
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL
-}));
+app.use(cors());
 
 app.use('/api', router);
 app.use('/columns', ColumnsRouter);
@@ -42,21 +42,26 @@ app.use('/user', UserRouter);
 const start = async () => {
     try {
         io.on('connection', function (socket) {
-            socket.on('CARD_ADD', async (data) => {
-                const newUser = await CardsController.newCard(data)
-                io.emit('CARD_ADDED', newUser)
-            })
-            socket.on('COLUMN_ADD', async (column) => {
-                console.log(column)
-                 socket.join(column.boardId)
-                //TODO
-                // const newColumn = await ColumnsController.newColumn(data)
-                    socket.broadcast.emit('COLUMN_ADDED', column)
-                // io.to(data.boardId).emit('COLUMN_ADDED', data)
+
+
+            socket.on('JOIN_BOARD', (BoardId)=>{
+                socket.join(BoardId)
+                console.log( `user ${socket.id} подписался на доску ${BoardId}`)
             })
 
+            socket.on('LEAVE_BOARD', (BoardId)=>{
+                socket.leave(BoardId)
+                console.log( `user ${socket.id} отписался от доски ${BoardId}`)
+            })
+
+            socket.on('COLUMN_ADD',  (column) => {
+                console.log(column)
+                // const newColumn = await ColumnsController.newColumn(data)
+                socket.in(column.boardId).emit('COLUMN_ADDED', column)
+            })
             console.log('A user connected', socket.id);
         });
+
 
 
         await mongoose.connect(process.env.DB_URL, {
